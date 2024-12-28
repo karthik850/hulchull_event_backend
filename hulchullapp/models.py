dc118c1209dbb09a979f7370edffe8c897140e69
karthik850 <karthikreddy.emireddy@gmail.com>
2024-12-28 10:50:27 +0530

    added gender field

--- hulchullapp/models.py
+++ hulchullapp/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 
 class SecretCodeDB(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
     fav_number = models.IntegerField()
     associate_name = models.CharField(max_length=255)
     user_name = models.CharField(max_length=255, null=True, blank=True)  # Allows user_name to be empty
     is_opened = models.BooleanField(default=False)
     opened_on = models.DateTimeField(null=True, blank=True)
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default='M',  # Optional: Set a default value
+    )
+    
 
     def __str__(self):
         return f"{self.associate_name} - {self.fav_number}"
